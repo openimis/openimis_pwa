42fe6fc820d3ffe9ef8b21903ccfda8a8ce30a5d
avanobberghen <alexandre.vanobberghen@gmail.com>
2018-11-24 16:15:43 +0100

    Started add family form

--- offline-insurance/pages/inspire.vue
+++ offline-insurance/pages/inspire.vue
@@ -2,14 +2,14 @@
   <v-layout>
     <v-flex text-xs-center>
       <img src="/v.png" alt="Vuetify.js" class="mb-5">
-      <blockquote class="blockquote">
+      <!-- <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
         <footer>
           <small>
             <em>&mdash;John Johnson</em>
           </small>
         </footer>
-      </blockquote>
+      </blockquote> -->
       <FormAddFamily></FormAddFamily>
     </v-flex>
   </v-layout>
