db300b2f27b685f38cc12a5e0e7804fee622b27c
Christopher Zimmermann <topher@dandelion.org>
2018-11-24 21:27:54 +0100

    fix formaddfamily lint errros.

--- offline-insurance/components/FormAddFamily.vue
+++ offline-insurance/components/FormAddFamily.vue
@@ -349,7 +349,18 @@ export default {
     items: ['Item 1', 'Item 2', 'Item 3', 'Item 4'],
     checkbox: false,
     district: null,
-    region: null
+    region: null,
+    beneficiaryCard: null,
+    educationLevels: null,
+    povertyStatus: null,
+    confirmationType: null,
+    ConfirmationNumber: null,
+    GroupType: null,
+    PermanentAddress: null,
+    InsuranceNumber: null,
+    FirstName: null,
+    LastName: null,
+    MaritalStatus: null
   }),
 
   computed: {
@@ -370,37 +381,46 @@ export default {
       return districts
     },
     checkboxErrors() {
-      // const errors = []
+      const errors = []
       // if (!this.$v.checkbox.$dirty) return errors
       // !this.$v.checkbox.checked && errors.push('You must agree to continue!')
-      // return errors
+      return errors
     },
     selectErrors() {
-      // const errors = []
+      const errors = []
       // if (!this.$v.select.$dirty) return errors
       // !this.$v.select.required && errors.push('Item is required')
-      // return errors
+      return errors
     },
     nameErrors() {
-      // const errors = []
+      const errors = []
       // if (!this.$v.name.$dirty) return errors
       // !this.$v.name.maxLength &&
       //   errors.push('Name must be at most 10 characters long')
       // !this.$v.name.required && errors.push('Name is required.')
-      // return errors
+      return errors
     },
     emailErrors() {
-      // const errors = []
+      const errors = []
       // if (!this.$v.email.$dirty) return errors
       // !this.$v.email.email && errors.push('Must be valid e-mail')
       // !this.$v.email.required && errors.push('E-mail is required')
-      // return errors
+      return errors
     }
   },
 
   methods: {
     submit() {
       this.$v.$touch()
+      console.log('this:' + this)
+      /*this.$axios
+        .post(
+          'https://baselhack.swisstph-mis.ch/RestApi/api/family/',
+          this
+        )
+        .then(console.warn)
+        .catch(console.error)
+        */
     },
     clear() {
       this.$v.$reset()
